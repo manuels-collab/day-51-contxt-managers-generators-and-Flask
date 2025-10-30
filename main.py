2d1558b8ca08a63560b4517364022d6bf87e1e58
Manuel Prince <manuelsdesk0029@gmail.com>
2025-10-30 10:51:28 +0100

    Day 61: I created a form

--- main.py
+++ main.py
@@ -1,48 +1,39 @@
-from mail import Mail
+from flask_bootstrap import Bootstrap5
 from flask import Flask, render_template, request
-import requests
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
 
 
-posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
+
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap5(app)
 
-@app.route('/')
-def get_all_posts():
-    return render_template("index.html", all_posts=posts)
-
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
+class MyForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(message="Fill in sth"), Email(check_deliverability=True, message="Invalid email")])
+    password = PasswordField(label="Password", validators=[DataRequired(), Length(min=8, message="Password must be more than eight characters")])
+    submit_button = SubmitField(label="Login")
 
 
-@app.route("/contact", methods=['GET'])
-def contact():
-    return render_template("contact.html")
+app.secret_key = 'secret_key'
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = MyForm()
+    if form.validate_on_submit():
+        print(request.url_rule.methods)
+        if form.email.data == 'manuels@gmail.com' and form.password.data == '12345678':
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html", form=form)
 
 
-@app.route("/post/<int:index>")
-def show_post(index):
-    requested_post = None
-    for blog_post in posts:
-        if blog_post["id"] == index:
-            requested_post = blog_post
-    return render_template("post.html", post=requested_post)
-
-@app.route("/contact", methods=['POST'])
-def receive_data():
-    msg = "Successfully sent the messages"
-    user_data = {
-        "name": request.form['name'],
-        'message': request.form['message'],
-        'phone_number': request.form['phone'],
-        'email': request.form['email'],
-    }
-    print(user_data)
-    mail = Mail(user_data['name'], user_data['message'], user_data['phone_number'], user_data['email'])
-    mail.load_email()
-    return render_template("contact.html", data = msg)
-if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+if __name__ == '__main__':
+    app.run(debug=True)
